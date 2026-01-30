7952bafbe10a332b4feb6492e47f260403f60413
Baudouin Christopher <cbaudouin@puydufou.com>
2026-01-30 12:35:44 +0100

    feat: config test + gitignore

---
+++ jest.config.ts
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'], // où chercher les fichiers de test
+};
